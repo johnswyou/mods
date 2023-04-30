2194d4eceef36ef1183fb7e352b4904f4e41a4b5
John You <johnswyou@gmail.com>
2023-04-30 14:48:05 -0400

    Update failure.py

--- src/mods/failure.py
+++ src/mods/failure.py
@@ -1,36 +1,31 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from mods.stress_transformation import principal_stress
 
-def von_mises(sigma_Y):
-    """
-    Plots the Von Mises failure envelope for a given yield strength.
+def plot_von_mises_failure_envelope(sy, sigma_x=None, sigma_y=None, tau_xy=None):
+    # Sy is the yield strength of the material
+    ellipse = Ellipse((0, 0), width=sy*np.sqrt(2)*2, height=sy*np.sqrt(2/3)*2, angle=45, alpha=0.4)
 
-    Parameters:
-        sigma_Y (float): Yield strength of the material
-
-    Returns:
-        None
-    """
-    # Set up the figure and axis
-    fig, ax = plt.subplots()
-
-    # Create an array of values for sigma_1
-    sigma_1 = np.linspace(0, 1.1 * sigma_Y, 1000)
-
-    # Calculate the corresponding values of sigma_2
-    sigma_2 = np.sqrt(sigma_Y**2 - sigma_1**2)
-
-    # Plot the Von Mises failure envelope
-    ax.plot(sigma_1, sigma_2, label='Von Mises')
-
-    # Add the yield strength to the plot
-    ax.axvline(x=sigma_Y, color='black', linestyle='--', label='Yield strength')
-
-    # Add labels and legend
-    ax.set_xlabel(r'$\sigma_1$')
-    ax.set_ylabel(r'$\sigma_2$')
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.add_patch(ellipse)
+    ax.set_xlim(-sy*np.sqrt(2), sy*np.sqrt(2))
+    ax.set_ylim(-sy*np.sqrt(2), sy*np.sqrt(2))
     ax.set_aspect('equal', adjustable='box')
-    ax.legend()
 
-    # Show the plot
+    # Draw bold black lines at x=0 and y=0
+    ax.axhline(0, color='black', linewidth=1.5)
+    ax.axvline(0, color='black', linewidth=1.5)
+
+    # If stress values are given, calculate and plot the principal stresses
+    if sigma_x is not None and sigma_y is not None and tau_xy is not None:
+        sigma_1, sigma_2 = principal_stress(sigma_x, sigma_y, tau_xy)
+        ax.plot(sigma_1, sigma_2, 'ro', markersize=8)
+    elif sigma_x is not None or sigma_y is not None or tau_xy is not None:
+        raise ValueError("Please provide all three stress values: sigma_x, sigma_y, and tau_xy")
+    
+    plt.title("Von Mises Failure Envelope for Plane Stress")
+    plt.xlabel("Major Principal Stress (σ1)")
+    plt.ylabel("Minor Principal Stress (σ2)")
+    plt.grid(True)
     plt.show()
