82d04aa154184c48f16cc1e8f2afcb7dcf8829f8
John You <johnswyou@gmail.com>
2023-04-30 13:06:03 -0400

    Update stress_transformation, add strain_transformation

---
+++ src/mods/strain_transformation.py
@@ -0,0 +1,226 @@
+import math
+import matplotlib.pyplot as plt
+
+def average_strain(epsilon_x, epsilon_y):
+    """
+    Calculates the average strain and returns the value.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+
+    Returns:
+        float: Average strain value
+    """
+    epsilon_avg = (epsilon_x + epsilon_y)/2
+
+    return epsilon_avg
+
+def normal_strain_transform(epsilon_x, epsilon_y, gamma_xy, theta):
+    """
+    Applies a strain transformation to the given strains and returns the transformed strain value.
+
+    Parameters:
+            epsilon_x (float): Strain value in the x-direction
+            epsilon_y (float): Strain value in the y-direction
+            gamma_xy (float): Shear strain value in the xy-plane
+            theta (float): Angle of rotation in degrees
+
+    Returns:
+        tuple: Transformed strain value in the x'-direction and y'-direction
+    """
+    theta_rad = math.radians(theta)
+    cos_theta = math.cos(2*theta_rad)
+    sin_theta = math.sin(2*theta_rad)
+
+    epsilon_x_prime = (epsilon_x + epsilon_y)/2 + (epsilon_x - epsilon_y)/2 * cos_theta + gamma_xy/2 * sin_theta
+    epsilon_y_prime = (epsilon_x + epsilon_y)/2 - (epsilon_x - epsilon_y)/2 * cos_theta - gamma_xy/2 * sin_theta
+
+    return epsilon_x_prime, epsilon_y_prime
+
+def shear_strain_transform(epsilon_x, epsilon_y, gamma_xy, theta):
+    """
+    Applies a transformation to the given shear strain and returns the transformed shear strain value.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+        theta (float): Angle of rotation in degrees
+
+    Returns:
+        float: Transformed shear strain value in the x'y'-plane
+    """
+    theta_rad = math.radians(theta)
+    cos_theta = math.cos(2*theta_rad)
+    sin_theta = math.sin(2*theta_rad)
+
+    gamma_xy_prime = -1*(epsilon_x - epsilon_y)/2 * sin_theta + gamma_xy/2 * cos_theta
+
+    return gamma_xy_prime
+
+def principal_strain(epsilon_x, epsilon_y, gamma_xy):
+    """
+    Calculates the principal strains and returns the values in a tuple.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+
+    Returns:
+        tuple: Principal strain values in the x'-direction and y'-direction
+    """
+    epsilon_prime = (epsilon_x + epsilon_y)/2
+    epsilon_double_prime = (epsilon_x - epsilon_y)/2
+    gamma_prime = gamma_xy/2
+
+    epsilon_1 = epsilon_prime + math.sqrt(epsilon_double_prime**2 + gamma_prime**2)
+    epsilon_2 = epsilon_prime - math.sqrt(epsilon_double_prime**2 + gamma_prime**2)
+
+    return epsilon_1, epsilon_2
+
+def principal_strain_angle(epsilon_x, epsilon_y, gamma_xy):
+    """
+    Calculates the principal directions and returns the values in a tuple.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+
+    Returns:
+        tuple: Principal direction values in degrees
+    """
+    epsilon_double_prime = (epsilon_x - epsilon_y)/2
+    gamma_prime = gamma_xy/2
+
+    theta_1 = math.degrees(math.atan2(gamma_prime, epsilon_double_prime))/2
+    theta_2 = theta_1 + 90
+
+    return theta_1, theta_2
+
+def maximum_in_plane_shear_strain(epsilon_x, epsilon_y, gamma_xy):
+    """
+    Calculates the maximum in-plane shear strain and returns the value.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+
+    Returns:
+        float: Maximum in-plane shear strain value
+    """
+    epsilon_double_prime = (epsilon_x - epsilon_y)/2
+    gamma_prime = gamma_xy/2
+
+    gamma_max = 2*math.sqrt(epsilon_double_prime**2 + gamma_prime**2)
+
+    return gamma_max
+
+def maximum_in_plane_shear_strain_angle(epsilon_x, epsilon_y, gamma_xy):
+    """
+    Calculates the angle of the maximum in-plane shear strain and returns the value.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+
+    Returns:
+        float: Angle of the maximum in-plane shear strain value in degrees
+    """
+    epsilon_double_prime = (epsilon_x - epsilon_y)/2
+    gamma_prime = gamma_xy/2
+
+    theta_max = math.degrees(math.atan2(-epsilon_double_prime, gamma_prime))/2
+
+    return theta_max, theta_max + 90
+
+def mohrs_circle(epsilon_x, epsilon_y, gamma_xy):
+    """
+    Calculates the center and radius of the Mohr's circle and returns the values in a tuple.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+
+    Returns:
+        tuple: Center and radius of the Mohr's circle
+    """
+    epsilon_prime = (epsilon_x + epsilon_y)/2
+    epsilon_double_prime = (epsilon_x - epsilon_y)/2
+    gamma_prime = gamma_xy/2
+
+    center = (epsilon_prime, 0)
+    radius = math.sqrt(epsilon_double_prime**2 + gamma_prime**2)
+
+    return center, radius
+
+def mohrs_circle_plot(epsilon_x, epsilon_y, gamma_xy):
+    """
+    Plots the Mohr's circle and returns the plot.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+
+    Returns:
+        matplotlib.pyplot.plot: Plot of the Mohr's circle
+    """
+    center, radius = mohrs_circle(epsilon_x, epsilon_y, gamma_xy)
+
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.set_aspect('equal')
+    ax.grid(True, which='both')
+    ax.axhline(y=0, color='k')
+    ax.axvline(x=0, color='k')
+    ax.set_xlabel(r'$\epsilon$')
+    ax.set_ylabel(r'$\gamma$')
+    ax.set_title("Mohr's Circle")
+
+    circle = plt.Circle(center, radius, color='b', fill=False)
+    ax.add_artist(circle)
+    return fig
+
+def mohrs_circle_strain(epsilon_x, epsilon_y, gamma_xy, theta):
+    """
+    Calculates the center and radius of the transformed Mohr's circle and returns the values in a tuple.
+
+    Parameters:
+        epsilon_x (float): Strain value in the x-direction
+        epsilon_y (float): Strain value in the y-direction
+        gamma_xy (float): Shear strain value in the xy-plane
+        theta (float): Angle of rotation in degrees
+
+    Returns:
+        tuple: Center and radius of the transformed Mohr's circle
+    """
+    epsilon_x_prime, epsilon_y_prime = normal_strain_transform(epsilon_x, epsilon_y, gamma_xy, theta)
+    gamma_xy_prime = shear_strain_transform(epsilon_x, epsilon_y, gamma_xy, theta)
+
+    return mohrs_circle_strain(epsilon_x_prime, epsilon_y_prime, gamma_xy_prime)
+
+def mohrs_circle_strain_plot(epsilon_x, epsilon_y, gamma_xy, theta):
+
+    epsilon_x_prime, gamma_xy_prime = mohrs_circle_strain(epsilon_x, epsilon_y, gamma_xy, theta)
+    center, radius = mohrs_circle(epsilon_x, epsilon_y, gamma_xy)
+    circle = plt.Circle((center, 0), radius, fill=False)
+    fig, ax = plt.subplots()
+    ax.set_aspect('equal')
+    ax.add_artist(circle)
+
+    # Set the x and y limits
+    ax.set_xlim([center - radius - 0.1 * radius, center + radius + 0.1 * radius])  # adding a little extra space
+    ax.set_ylim([-radius - 0.1 * radius, radius + 0.1 * radius])  # adding a little extra space
+
+    plt.plot(epsilon_x_prime, gamma_xy_prime, 'ro')
+    plt.xlabel("Normal Strain")
+    plt.ylabel("Shear Strain")
+    plt.title("Mohr's Circle")
+    plt.grid()
+    plt.show()
+    return fig
--- src/mods/stress_transformation.py
+++ src/mods/stress_transformation.py
@@ -26,11 +26,12 @@ def normal_stress_transform(sigma_x, sigma_y, tau_xy, theta):
         theta (float): Angle of the inclined plane in degrees.
 
     Returns:
-        float: Normal stress on the inclined plane.
+        tuple: Normal stress on the inclined plane in the x' and y' directions.
     """
     theta_rad = math.radians(theta)  # converting degrees to radians
-    sigma_n = (sigma_x + sigma_y)/2 + (sigma_x - sigma_y)/2*math.cos(2*theta_rad) + tau_xy*math.sin(2*theta_rad)
-    return sigma_n
+    sigma_x_prime = (sigma_x + sigma_y)/2 + (sigma_x - sigma_y)/2*math.cos(2*theta_rad) + tau_xy*math.sin(2*theta_rad)
+    sigma_y_prime = (sigma_x + sigma_y)/2 - (sigma_x - sigma_y)/2*math.cos(2*theta_rad) - tau_xy*math.sin(2*theta_rad)
+    return sigma_x_prime, sigma_y_prime
 
 def shear_stress_transform(sigma_x, sigma_y, tau_xy, theta):
     """
@@ -49,7 +50,7 @@ def shear_stress_transform(sigma_x, sigma_y, tau_xy, theta):
     tau_n = -(sigma_x - sigma_y)/2*math.sin(2*theta_rad) + tau_xy*math.cos(2*theta_rad)
     return tau_n
 
-def principal_stress(sigma_x, sigma_y, tau_xy, stress_type):
+def principal_stress(sigma_x, sigma_y, tau_xy):
     """
     Calculate the principal stress.
 
@@ -60,18 +61,11 @@ def principal_stress(sigma_x, sigma_y, tau_xy, stress_type):
         stress_type (str): 'max' for major principal stress, 'min' for minor principal stress.
 
     Returns:
-        float: Principal stress, either major or minor based on stress_type.
-
-    Raises:
-        ValueError: If stress_type is not 'max' or 'min'.
+        tuple: Major and minor principal stresses.
     """
-    if stress_type == "max":
-        sigma = (sigma_x + sigma_y)/2 + math.sqrt(((sigma_x - sigma_y)/2)**2 + tau_xy**2)
-    elif stress_type == "min":
-        sigma = (sigma_x + sigma_y)/2 - math.sqrt(((sigma_x - sigma_y)/2)**2 + tau_xy**2)
-    else:
-        raise ValueError("Invalid stress_type. Choose either 'max' or 'min'.")
-    return sigma
+    sigma_1 = (sigma_x + sigma_y)/2 + math.sqrt(((sigma_x - sigma_y)/2)**2 + tau_xy**2)
+    sigma_2 = (sigma_x + sigma_y)/2 - math.sqrt(((sigma_x - sigma_y)/2)**2 + tau_xy**2)
+    return sigma_1, sigma_2
 
 def principal_stress_angle(sigma_x, sigma_y, tau_xy):
     """
@@ -89,7 +83,8 @@ def principal_stress_angle(sigma_x, sigma_y, tau_xy):
     denominator = sigma_x - sigma_y
     theta_p_rad = math.atan2(numerator, denominator) / 2  # atan2 takes care of the angle quadrant
     theta_p_deg = math.degrees(theta_p_rad)  # converting radians to degrees
-    return theta_p_deg
+    
+    return theta_p_deg, theta_p_deg + 90
 
 def maximum_in_plane_shear_stress(sigma_x, sigma_y, tau_xy):
     """
@@ -122,7 +117,8 @@ def maximum_in_plane_shear_stress_angle(sigma_x, sigma_y, tau_xy):
     denominator = 2 * tau_xy
     theta_max_rad = math.atan2(numerator, denominator) / 2  # atan2 takes care of the angle quadrant
     theta_max_deg = math.degrees(theta_max_rad)  # converting radians to degrees
-    return theta_max_deg
+    
+    return theta_max_deg, theta_max_deg + 90
 
 def mohrs_circle(sigma_x, sigma_y, tau_xy):
     """
